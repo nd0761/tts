64324d0fdbf1a012c2c34083daf3605605d12d83
nd0761 <deni,nat.and@gmail.com>
2021-12-05 18:22:26 +0300

    Refactor config

--- utils/config.py
+++ utils/config.py
@@ -4,14 +4,15 @@ import dataclasses
 
 @dataclasses.dataclass
 class TaskConfig:
-    work_dir: str = "/home/jupyter/work/resources/tts"  # because we work with ds
-    work_dir_LJ: str = "/home/jupyter/mnt/s3/bucket-hse-rw/data/datasets"  # because we work with ds
-    model_path: str = "/home/jupyter/work/resources/models"
+    work_dir: str = "/home/jupyter/work/resources/tts"  # tts directory
+    work_dir_LJ: str = "/home/jupyter/mnt/s3/bucket-hse-rw/data/datasets"  # directory with LJ dataset
+    model_path: str = "/home/jupyter/work/resources/models"  # directory with model results (contain best model and last epoch model)
 
-    aligner: str = "fsa"
+    aligner: str = "fsa"  # type of aligner fsa - pretrained Fast Speech Aligner; ga - Grapheme Aligner
 
-    one_batch: bool = False
-    PAD_IDX: int = 0
+    one_batch: bool = False  # if you want to train just on one batch
+
+    # Config for Fast Speech model and its blocks
 
     vocab_size: int = 10000
     mels: int = 80
@@ -42,23 +43,22 @@ class TaskConfig:
 
     output_linear_in_dim: int = 384
 
-    torch_seed: int = 42
+    torch_seed: int = 42  # set torch seed for reproduction purpose
 
-    batch_size: int = 3
+    batch_size: int = 3  # set train batchsize, validation batchsize will be calculated as the number of lines in test_text.txt
 
-    batch_limit: int = -1
+    batch_limit: int = -1  # set number of batches for training (by setting it -1 all batches will be used)
 
-    learning_rate: float = 4e-4
-    weight_decay: float = 1e-5
+    learning_rate: float = 4e-4  # max_lr for scheduler
+    weight_decay: float = 1e-5  #weight_decay for optimizer
 
-    num_epochs: int = 750
+    num_epochs: int = 750  # number of epochs
+
     device: torch.device = torch.device(
-        'cuda:0' if torch.cuda.is_available() else 'cpu')
-    wandb_api: str = "99f2c4dae0db3099861ebd92a63e1194f42d16d9"
-    wandb_project: str = "tts-final"
-    log_audio: bool = True
-    laep: int = 107
-    laep_model: int = 1
-    laep_val: int = 1
-    log_final_audio: bool = True
-    wandb: bool = True
+        'cuda:0' if torch.cuda.is_available() else 'cpu')  # set device
+    wandb_project: str = "tts-final"  # set wandb project name
+    log_audio: bool = True  # True if you want to log audio results while training
+    laep: int = 107  # Data will be sent to wandb every laep iterations
+    laep_model: int = 1  # Model will be saved every laep_model iterations (best model will be saved after every improvement)
+    laep_val: int = 1  # Validation will be called every laep_val iterations during training
+    wandb: bool = True  # Set False if you Don't want to send any data to wandb
